b8cec7e9373d721764aa8e7af80edd2263150303
Matheus <mat.araujoribeiro@gmail.com>
2023-10-17 12:49:56 -0300

    string comparisons

--- Program.cs
+++ Program.cs
@@ -7,9 +7,22 @@ namespace BaltaCSharp;
   class Program
   {
       public static void Main(string[] args)
-      {        
-        var id = Guid.NewGuid();
-        System.Console.WriteLine(id);
+      { 
+
+
+        var text = "testando";
+        var text2 = "Testando";
+        System.Console.WriteLine(text.Contains(text2, StringComparison.OrdinalIgnoreCase));
+
+        // var price = 10.2;
+        // var price2 = 11.6;
+        // var text = string.Format("O preço do produto é {0} apenas na promoção, ou {1}", price, price2);
+        // var text2 = $"O preço do produto é {price}";
+        // System.Console.WriteLine(text2);
+
+        // var id = Guid.NewGuid();
+        // System.Console.WriteLine(id);
+
         // var product = new Product();
         // product.Price = 29.99;
         // System.Console.WriteLine(product.Id);
@@ -19,26 +32,26 @@ namespace BaltaCSharp;
       
   }
 
-  struct Product{
-    public Product(int id, string name, double price, EProductType type){
-      Id = id;
-      Name = name;
-      Price = price;
-      Type = type;
-    }
-    public int Id;
-    public string Name = string.Empty;
-    public double Price;
+  // struct Product{
+  //   public Product(int id, string name, double price, EProductType type){
+  //     Id = id;
+  //     Name = name;
+  //     Price = price;
+  //     Type = type;
+  //   }
+  //   public int Id;
+  //   public string Name = string.Empty;
+  //   public double Price;
 
-    public EProductType Type;
-    public double PriceInDolar(double dolar)
-    {
-      return Price * dolar;
-    }
-  }
+  //   public EProductType Type;
+  //   public double PriceInDolar(double dolar)
+  //   {
+  //     return Price * dolar;
+  //   }
+  // }
   
-  enum EProductType
-  {
-    Product = 1,
-    Service = 2
-  } 
+  // enum EProductType
+  // {
+  //   Product = 1,
+  //   Service = 2
+  // } 
