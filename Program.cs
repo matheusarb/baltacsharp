9703e14508750f81752a7ec866a3689de9eb540f
Matheus <mat.araujoribeiro@gmail.com>
2023-10-23 14:08:16 -0300

    string methods

--- Program.cs
+++ Program.cs
@@ -9,11 +9,14 @@ namespace BaltaCSharp;
       public static void Main(string[] args)
       { 
 
-
         var text = "testando";
         var text2 = "Testando";
         System.Console.WriteLine(text.Contains(text2, StringComparison.OrdinalIgnoreCase));
+        Console.WriteLine(text.Equals(text2));
 
+        var texto = "Este texto é um teste";
+        Console.WriteLine(texto.LastIndexOf("e"));
+        Console.WriteLine(texto.IndexOf("um"));
         // var price = 10.2;
         // var price2 = 11.6;
         // var text = string.Format("O preço do produto é {0} apenas na promoção, ou {1}", price, price2);
