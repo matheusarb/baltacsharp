fbf3a10a0ca0676f40899bac3ac29d956a48e347
Matheus <mat.araujoribeiro@gmail.com>
2023-10-30 00:50:57 -0300

    dates

---
+++ Dates/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Dates;
+
+class Program
+{
+  static void Main(string[] args)
+  {
+    Console.Clear();
+    // var data = new DateTime();
+    var dataHoje = DateTime.Now;
+    System.Console.WriteLine(dataHoje);
+  }
+} 
