9125de1ba9fd6afa581173cb474e24f219859d5a
Matheus <mat.araujoribeiro@gmail.com>
2023-10-31 10:47:10 -0300

    datas

--- Dates/Program.cs
+++ Dates/Program.cs
@@ -7,8 +7,29 @@ class Program
   static void Main(string[] args)
   {
     Console.Clear();
-    // var data = new DateTime();
-    var dataHoje = DateTime.Now;
-    System.Console.WriteLine(dataHoje);
+
+    // ObterDatas();
+    FormatarData();
   }
+
+  private static void ObterDatas()
+  {
+    var data = new DateTime(2023, 10, 31, 10, 15, 02);
+    // var dataHoje = DateTime.Now;
+    System.Console.WriteLine(data.ToString());
+    System.Console.WriteLine(data.DayOfWeek);
+    System.Console.WriteLine((int)data.DayOfYear);
+    // System.Console.WriteLine(data);
+    // System.Console.WriteLine(data);
+    // System.Console.WriteLine(data);
+  }
+
+  private static void FormatarData()
+  {
+    var data = DateTime.Now;
+
+    var dataFormatada = String.Format("{0:y}", data);
+    System.Console.WriteLine(dataFormatada);
+  }
+
 } 
